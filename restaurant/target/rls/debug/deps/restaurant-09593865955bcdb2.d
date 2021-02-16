c56d4296f890cd41295cd9d82f3578c34999d7c8
Ari Firmanto <arifirmanto@gmail.com>
2021-02-16 14:04:17 +0700

    remove target restaurant

--- restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.d
+++
@@ -1,5 +0,0 @@
-/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.rmeta: src/lib.rs
-
-/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.d: src/lib.rs
-
-src/lib.rs:
