17a26f3094f2f537b26dd390a4450cc400fcca3d
Ari Firmanto <arifirmanto@gmail.com>
2021-02-16 13:25:04 +0700

    demo module libraries for restaurant

---
+++ restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.d
@@ -0,0 +1,5 @@
+/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.rmeta: src/lib.rs
+
+/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-09593865955bcdb2.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/rls/debug/deps/restaurant-443a43602eef3c04.d
@@ -0,0 +1,5 @@
+/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-443a43602eef3c04.rmeta: src/lib.rs
+
+/Users/w6-mbp/Documents/work/rust/restaurant/target/rls/debug/deps/restaurant-443a43602eef3c04.d: src/lib.rs
+
+src/lib.rs:
